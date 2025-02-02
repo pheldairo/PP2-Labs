6da6dcc77f5c5d8f7e1bacd33b7411de9d9b51eb
Daniil Bodnar <lephiador@gmail.com>
2025-02-02 14:39:59 +0500

    Welcome back, functions folder

--- Lab3/functions/functions 1/1.py
+++ Lab3/functions/functions 1/1.py
@@ -2,6 +2,5 @@ def grams_to_ounces(grams):
     ounces = 28.3495231 * grams
     return ounces
 
-# Example usage:
 grams = int(input("Enter grams: "))
 print(f"{grams} grams is equal to {grams_to_ounces(grams)} ounces")
